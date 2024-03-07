2baaa2de7984b449bbf863ffa0882d8fdc425469
PRATHAM <codermaster2004@gmail.com>
2024-03-08 01:21:23 +0530

    Initial commit

--- app/src/main/java/com/example/tictactoeapp/FirstPageActivity.java
+++ app/src/main/java/com/example/tictactoeapp/FirstPageActivity.java
@@ -6,10 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class FirstPageActivity extends AppCompatActivity {
 
     Button btnlogin;
+
+    ImageView fingerprint;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +20,19 @@ public class FirstPageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_first_page);
 
         btnlogin = findViewById(R.id.btnlogin);
+        fingerprint = findViewById(R.id.fingerprint);
 
         btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(FirstPageActivity.this,MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        fingerprint.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
